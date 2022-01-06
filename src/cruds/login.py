81da3db36bf820b6c8eb54ca6afe137e90d3e96d
kami9811 <49345024+kami9811@users.noreply.github.com>
2022-01-07 08:16:18 +0900

    Update login.py
    Fixed filter function.

--- src/cruds/login.py
+++ src/cruds/login.py
@@ -5,4 +5,4 @@ from src.schemas import *
 
 def login(db: Session, login: Login):
   # return login.email
-  return db.query(User).filter(User.email == login.email and User.hashed_password == login.password).first()
+  return db.query(User).filter(User.email == login.email).filter(User.hashed_password == login.password).first()
