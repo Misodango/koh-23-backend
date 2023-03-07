2797114f1dfc1ea0b7ba8c63e4b6cc9504931ca0
yuta.kawakami <kami.981108@gmail.com>
2023-03-08 04:33:49 +0900

    Fixed for ngrok wrapper

--- src/ngrok/settings.py
+++ src/ngrok/settings.py
@@ -16,22 +16,23 @@ def init_webhooks(base_url):
       pass
 
 def setup_ngrok():
-  settings = Settings()
+    settings = Settings()
 
-  if settings.USE_NGROK:
-      # pyngrok should only ever be installed or initialized in a dev environment when this flag is set
-      from pyngrok import ngrok
+    if settings.USE_NGROK:
+        # pyngrok should only ever be installed or initialized in a dev environment when this flag is set
+        from pyngrok import conf, ngrok
 
-      # Get the dev server port (defaults to 8000 for Uvicorn, can be overridden with `--port`
-      # when starting the server
-      port = sys.argv[sys.argv.index("--port") + 1] if "--port" in sys.argv else 8000
+        # Get the dev server port (defaults to 8000 for Uvicorn, can be overridden with `--port`
+        # when starting the server
+        port = sys.argv[sys.argv.index("--port") + 1] if "--port" in sys.argv else 8000
 
-      # Open a ngrok tunnel to the dev server
-      public_url = ngrok.connect(port).public_url
-      logger.info("ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
+        # Open a ngrok tunnel to the dev server
+        conf.get_default().auth_token = os.environ.get("NGROK_AUTH_TOKEN")
+        public_url = ngrok.connect(port).public_url
+        logger.info("ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
 
-      # Update any base URLs or webhooks to use the public ngrok URL
-      settings.BASE_URL = public_url
-      init_webhooks(public_url)
+        # Update any base URLs or webhooks to use the public ngrok URL
+        settings.BASE_URL = public_url
+        init_webhooks(public_url)
   
-  return settings
+    return settings
